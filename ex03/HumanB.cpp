0e3f7f34d7714762d4d3037c79acaffa8fdd9623
Mohaben-1 <mohammedwhb2001@gmail.com>
2025-09-02 11:33:40 +0100

    CPP Module 01

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: mohaben- <mohaben-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 19:18:26 by mohaben-          #+#    #+#             */
-/*   Updated: 2025/07/25 19:43:09 by mohaben-         ###   ########.fr       */
+/*   Updated: 2025/09/02 11:32:50 by mohaben-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : name(name), weapon(nullptr) {}
+HumanB::HumanB(std::string name) : name(name), weapon(NULL) {}
 
 void	HumanB::setWeapon(Weapon& weapon)
 {
